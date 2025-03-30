30195318cc446ebdd9cc04fdae30bdffef5249a7
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-03-30 15:18:24 +0200

    env vars test

--- components/sections/installation.tsx
+++ components/sections/installation.tsx
@@ -21,7 +21,7 @@ export default function Installation() {
 
   const BASE_URL =
     process.env.VERCEL_ENV === "production"
-      ? `https://${process.env.VERCEL_URL}`
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
       : `http://localhost:3000`;
 
   const installCommand = `curl -sS ${BASE_URL}/install.sh | sh`;
